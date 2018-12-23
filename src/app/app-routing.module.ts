fb1d000ae1dc70acca6be3316f3f1622da2388e9
linx000 <sbalusu2@ufl.edu>
2018-12-23 18:04:46 -0500

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'appointments', loadChildren: './appointments/appointments.module#AppointmentsPageModule' },
+  { path: 'orders', loadChildren: './orders/orders.module#OrdersPageModule' },
+  { path: 'prescriptions', loadChildren: './prescriptions/prescriptions.module#PrescriptionsPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/appointments/appointments.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appointments',
+  templateUrl: './appointments.page.html',
+  styleUrls: ['./appointments.page.scss'],
+})
+export class AppointmentsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  hero = "winston"
+
+}
